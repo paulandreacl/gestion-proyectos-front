84012650d6b56694850be0472557fff0adace736
paulandreacl <88589288+paulandreacl@users.noreply.github.com>
2021-12-06 23:39:12 -0500

    Feature paula (#2)
    * login
    
    * vruta privada usuarios

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -8,8 +8,10 @@ import { Link } from 'react-router-dom';
 import { REGISTRO } from 'graphql/auth/mutations'
 import { useMutation } from '@apollo/client';
 import { useNavigate } from 'react-router';
+import { useAuth } from 'context/authContext';
 
 const Register = () => {
+  const { setToken } = useAuth();
   const navigate = useNavigate();
   const { form, formData, updateFormData } = useFormData(null);
 
@@ -18,19 +20,17 @@ const Register = () => {
 
   const submitForm = (e) => {
     e.preventDefault();
-    console.log('enviar datos al backend', formData);
     registro({ variables: formData });
   };
 
   useEffect(() => {
-    console.log('data mutation', dataMutation);
     if (dataMutation) {
       if (dataMutation.registro.token) {
-        localStorage.setItem('token', dataMutation.registro.token);
+        setToken(dataMutation.registro.token);
         navigate('/');
       }
     }
-  }, [dataMutation]);
+  }, [dataMutation, setToken, navigate]);
 
   return (
     <div className='flex flex-col h-full w-full items-center justify-center'>
